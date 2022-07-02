b3345b846d4a4f9816d02238e9f4166546c45588
mattias <mattias.decharleroy@gmail.com>
2022-07-02 21:07:34 +0200

    Implement redis ohlc fetcher and use it before using Yahoo ohlc fetcher

--- stock_info_api/main.py
+++ stock_info_api/main.py
@@ -8,6 +8,7 @@ from stock_market.ext.fetcher import YahooOHLCFetcher
 
 from .config import get_settings
 from .redis import init_redis_pool
+from .redis_ohlc_fetcher import RedisOHLCFetcher
 
 app = FastAPI(title="Stock Info API")
 
@@ -34,10 +35,15 @@ async def startup_event():
     app.state.redis = init_redis_pool()
 
 
+def create_fetcher(app):
+    return RedisOHLCFetcher(app.state.redis, YahooOHLCFetcher())
+
+
 @app.post("/ohlc")
 async def ohlc(requests: RequestsModel):
-    fetcher = YahooOHLCFetcher()
+    fetcher = create_fetcher(app)
     ohlc_results = await fetcher.fetch_ohlc(
         ((r.start_date, r.end_date, r.ticker.create()) for r in requests.requests)
     )
+    # print(list(ohlc_results))
     return [(t.to_json(), o.to_json()) for t, o in ohlc_results]
---
+++ stock_info_api/redis_ohlc_fetcher.py
@@ -0,0 +1,38 @@
+import asyncio
+
+from stock_market.core import OHLC, OHLCFetcher
+from stock_market.core.ohlc import merge_ohlcs
+
+
+class RedisOHLCFetcher(OHLCFetcher):
+    def __init__(self, redis, delegate):
+        super().__init__("redis")
+        self.redis = redis
+        self.delegate = delegate
+
+    async def fetch_from_cache(self, ticker):
+        ohlc_json = await self.redis.get(ticker.symbol)
+        return None if ohlc_json is None else OHLC.from_json(ohlc_json)
+
+    async def fetch_single(self, start_date, end_date, ticker):
+        ohlc = await self.fetch_from_cache(ticker)
+
+        if ohlc is None or ohlc.start > start_date or ohlc.end < end_date:
+            fetched_ohlc = await self.delegate.fetch_ohlc(
+                [(start_date, end_date, ticker)]
+            )
+            if fetched_ohlc is None:
+                return ticker, None
+            assert len(fetched_ohlc) == 1
+
+            ohlc = merge_ohlcs(ohlc, fetched_ohlc[0][1])
+
+            # should we asyncio.create_task instead of await?
+            await self.redis.set(ticker.symbol, ohlc.to_json())
+        return ticker, ohlc
+
+    async def fetch_ohlc(self, requests):
+        tasks = []
+        for request in requests:
+            tasks.append(self.fetch_single(*request))
+        return await asyncio.gather(*tasks)
